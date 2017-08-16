c76913ee07a49657b87ef1ff33121906eccd1e16
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-16 14:58:39 -0400

    change from lookup by index to username

--- app.js
+++ app.js
@@ -13,12 +13,18 @@ app.get('/index/', function (req, res) {
   res.render('index', data);
 });
 
+app.get("/index/:username", function (req, res) {
+ let thisUser = data.users.filter(function( obj ) {
+    return obj.username == req.params.username;
+  });
+    res.render('profile',thisUser[0])
+})
 
+/*
 app.get('/index/:requestedid', function (req, res) {
   var useridex = req.params.requestedid - 1;
   res.render('profile', data.users[useridex]);
-});
-
+});*/
 
 app.listen(port, function(){
   console.log("Server active on http://localhost:3000/");
