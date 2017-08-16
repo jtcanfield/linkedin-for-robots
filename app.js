97b6721320e6cc28e828d7e47d505777eca3f703
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-16 10:47:09 -0400

    add function to pull api

--- app.js
+++ app.js
@@ -30,3 +30,65 @@ app.get("/:dynamic", function (req, res){
 app.listen(port, function(){
   console.log("Server active on http://localhost:3000/");
 });
+searchSubmit();
+
+function searchSubmit(){
+  let usersearch = $("#textinput").val()
+  fetch("https://tiy-learn-content.s3.amazonaws.com/36d3402e-data.js")
+  .then(
+    function(response) {
+      if (response.status !== 200) {
+        console.log(response.status);
+        return;
+      }
+      response.json().then(function(data) {
+        console.log(data);
+      });
+    }
+  )
+  .catch(function(err) {
+    console.log("Fetch Error: ", err);
+  });
+}
+
+
+/*
+
+
+function searchSubmit(){
+  let usersearch = $("#textinput").val()
+  fetch("http://recipepuppyproxy.herokuapp.com/api/?q=" + usersearch)
+    .then(
+      function(response) {
+        if (response.status !== 200) {
+          console.log(response.status);
+          return;
+        }
+        response.json().then(function(data) {
+          let resultsArray = data.results;
+          let parentbody = document.querySelector('#searchappender');
+          parentbody.innerHTML = "";
+          resultsArray.map((i) =>{
+            console.log(i);
+            let parentDiv = document.createElement('div');
+            parentbody.appendChild(parentDiv);
+            let backgroundimg = document.createElement('p');
+            if (i.thumbnail !== ""){
+              backgroundimg.setAttribute("style", "background-image: url("+i.thumbnail+");");
+            }
+            let spantext = document.createElement('a');
+            spantext.innerHTML = i.title + "<br>Ingredients: " + i.ingredients + "<br><br><br>";
+            spantext.setAttribute("href", i.href);
+            parentDiv.appendChild(backgroundimg);
+            parentDiv.appendChild(spantext);
+          })
+        });
+      }
+    )
+    .catch(function(err) {
+      console.log("Fetch Error: ", err);
+    });
+}
+
+
+*/
