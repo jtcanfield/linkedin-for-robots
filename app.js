9897ca20986b9d9a6ee20d509f1468c41a40f428
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-08-28 15:20:39 -0400

    Add search options

--- app.js
+++ app.js
@@ -3,24 +3,18 @@ const app = express();
 const port = 3000;
 const mustacheExpress = require('mustache-express');
 const data = require('./data.js');
+const bodyParser = require('body-parser');
 app.use(express.static(__dirname + '/public'));
 app.engine('mustache', mustacheExpress());
 app.set('views', './views');
 app.set('view engine', 'mustache');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.text());
 const mongodb = require('mongodb');
 const MongoClient = mongodb.MongoClient;
-const mongoURL = 'mongodb://localhost:27017/newdb';
+const mongoURL = 'mongodb://localhost:27017/newdbNAME';
 
-app.use('/', function (req, res) {
-  MongoClient.connect(mongoURL, function (err, db) {
-    const restaurants = db.collection('restaurants');
-    console.log(db);
-    restaurants.find({}).toArray(function (err, docs) {
-      console.log(docs);
-      res.render("index", {restaurants: docs})
-    })
-  })
-})
 
 // renders all data on the /index page
 app.get('/index/', function (req, res) {
@@ -44,8 +38,34 @@ app.listen(port, function(){
   console.log("Server active on http://localhost:3000/");
 });
 
+app.use('/searchavailforhire', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robo = db.collection("robots");
+    robo.find({job:null}).toArray(function (err, docs) {
+      res.render("index", {users: docs})
+    })
+  })
+})
+app.use('/searchwithjobs', function (req, res) {
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robo = db.collection('robots');
+    robo.find({job:{$ne: null}}).toArray(function (err, docs) {
+      res.render("index", {users: docs})
+    })
+  })
+})
+app.use('/searchname', function (req, res) {
+  var searched = req.body.namesearch;
+  MongoClient.connect(mongoURL, function (err, db) {
+    const robo = db.collection('robots');
+    robo.find({name:searched}).toArray(function (err, docs) {
+      res.render("index", {users: docs})
+    })
+  })
+})
+
 // Use connect method to connect to the server
 MongoClient.connect(mongoURL, function(err, db) {
-  console.log("Connected successfully to server");
+  console.log("Connected successfully to server at " + mongoURL);
   db.close();
 });
